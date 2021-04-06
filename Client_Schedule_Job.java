acd6fdc99bf8a7612382a0f1245a4604a4018290
jonathancawood <68835478+jonathancawood@users.noreply.github.com>
2021-04-06 15:26:18 +1000

    Planning

--- Client_Schedule_Job.java
+++ Client_Schedule_Job.java
@@ -1,1 +1,14 @@
+//Send HELO
+//Receive reply 
 
+//Send AUTH
+//Recieve reply 
+
+//Send REDY
+//Recieve reply
+
+//Send GETS ALL
+//Recieves reply from GETS ALL
+//Send OK
+
+//The reply will be infomation data from the server
