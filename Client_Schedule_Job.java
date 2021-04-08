dbb578326a689d23d5e09c9b56f12d0a47f73645
jonathancawood <68835478+jonathancawood@users.noreply.github.com>
2021-04-09 09:39:35 +1000

    Implemented REDY

--- Client_Schedule_Job.java
+++ Client_Schedule_Job.java
@@ -1,50 +1,115 @@
 import java.net.*;
+
 import java.io.*;
+
 import java.nio.charset.StandardCharsets;
+
 //import java.util.ArrayList;
 
+
+
 public class Client_Schedule_Job {
+
     public static void main(String args[]){
+
         try{
+
             Socket s = new Socket("127.0.0.1", 50000);
+
             DataInputStream din = new DataInputStream(s.getInputStream());
+
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+
             BufferedOutputStream bout = new BufferedOutputStream(dout);
+
             BufferedInputStream bin = new BufferedInputStream(din);
+
         
+
             //Send HELO
+
             String HELO ="HELO";
+
             bout.write(HELO.getBytes());
+
             System.out.println("HELO has been sent to server");
+
             bout.flush();
 
+
+
             //Receive reply from HELO
+
             byte[] serverReplyHELO = new byte[32];
+
             bin.read(serverReplyHELO);
+
             String ServerReplyHELO = new String(serverReplyHELO, StandardCharsets.UTF_8);
+
             System.out.println("RCVD in response to HELO: "+ ServerReplyHELO);
+
             
+
             //Send AUTH
+
+            String AUTH = "AUTH BLANK";
+
+            bout.write(AUTH.getBytes());
+
+            System.out.println("AUTH has been sent to server");
+
+            bout.flush();
+
+
 
             //Recieve reply from AUTH
 
+            byte[] serverReplyAUTH = new byte[32];
+
+            bin.read(serverReplyAUTH);
+
+            String ServerReplyAUTH = new String(serverReplyAUTH, StandardCharsets.UTF_8);
+
+            System.out.println("RCVD in response to AUTH: " + ServerReplyAUTH);
+
+
+
             //Send REDY
+
+
 
             //Recieve reply from REDY
 
+
+
             //Send GETS ALL
+
+
 
             //Recieves reply from GETS ALL
 
+
+
             //Send OK
 
+
+
             
+
             //The reply will be infomation data from the server
+
         
+
             bout.close();
+
             s.close();
+
         }catch (Exception e){
+
             System.out.println(e);
+
         }
+
     }
+
 }
