cea0e5ef9b11db19eb2d15b41066da1efda5b897
jonathancawood <68835478+jonathancawood@users.noreply.github.com>
2021-04-09 09:02:39 +1000

    In/Out Stream Setup

--- Client_Schedule_Job.java
+++ Client_Schedule_Job.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
+import java.net.*;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class Client_Schedule_Job {
+    public static void main(String args[]){
+        try{
+            Socket s = new Socket("127.0.0.1", 50000);
+            DataInputStream din = new DataInputStream(s.getInputStream());
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            BufferedOutputStream bout = new BufferedOutputStream(bout);
+            BufferedInputStream bin = new BufferedInputStream(bin);
+
+
+            bout.close();
+            s.close();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
 //Send HELO
 //Receive reply 
 
