48d75ec748e3961e4208e5ca99751970a7a81c46
jonathancawood <68835478+jonathancawood@users.noreply.github.com>
2021-04-13 16:31:26 +1000

    Trying to import servers into array list
    had to create a class in order to make an array list of class servers.

--- Client_Schedule_Job.java
+++ Client_Schedule_Job.java
@@ -2,8 +2,18 @@ import java.net.*;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 //import java.util.ArrayList;
+class Servers{
+    String serverName;
+    int serverID;
+    String state;
+    int currStartTime;
+    int cores;
+    int mem;
+    int disk;
+}
 
 public class Client_Schedule_Job {
+    public ArrayList<Servers> serverList= new ArrayList<Servers>();
     public static void main(String args[]){
         try{
             Socket s = new Socket("127.0.0.1", 50000);
@@ -64,7 +74,35 @@ public class Client_Schedule_Job {
             String OK ="OK";
             bout.write(OK.getBytes());
             bout.flush();
+            
+            //get the server reply and convert to string spliting along the way
+            String[] temp = ServerReplyGETS.split(" ");
+            byte[] serverReplyGETS1 = new byte[Integer.parseInt(temp[1])*Integer.parseInt(temp[2])*Integer.parseInt(temp[3])*Integer.parseInt(temp[4])*Integer.parseInt(temp[5])*Integer.parseInt(temp[6])];
+            String ServerReplyGETS1 = new String(serverReplyGETS1, StandardCharsets.UTF_8);
+            
+            //convert the strings intro an array of strings
+            String[] arrofstr = ServerReplyGETS1.split("\n");   
+            Client_Schedule_Job cjs = new Client_Schedule_Job();
 
+            // go throught the array of strings and for each reply create a server and import the right values into the right fields, repeating this for all servers in the reply
+            for(String server: arrofstr){
+                String[] indiServer = server.split(" ");
+                Servers Indi = new Servers();
+                Indi.serverName= indiServer[0];
+                Indi.serverID = Integer.parseInt(indiServer[1]);
+                Indi.state = indiServer[2];
+                Indi.currStartTime = Integer.parseInt(indiServer[3]);
+                Indi.cores = Integer.parseInt(indiServer[4]);
+                Indi.mem = Integer.parseInt(indiServer[5]);
+                Indi.disk = Integer.parseInt(indiServer[6]);
+                cjs.serverList.add(Indi);
+            }
+            //send ok to the server
+            bout.write(OK.getBytes());
+            System.out.println("OK has been send to server again");
+            bout.flush();
+            
+            
             //The reply will be infomation data from the server
             bout.close();
             s.close();
